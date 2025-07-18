809f79ad6a99508529ace41de4be4247637d2844
mrdeveloper917 <thakurayush995537@gmail.com>
2025-07-18 23:22:35 +0530

    add some code

--- public/script.js
+++ public/script.js
@@ -7,5 +7,13 @@ document.getElementById('registerForm').addEventListener('submit', async(e) =>{
     document.getElementById("email").value ;
     const password =
     document.getElementById("password").value ;
-    
-})
+    const response = await fetch ("/api/register", {
+        method : "POST",
+        headers : { "Content-Type": "application/json "},
+        body : JSON.stringify({ name,email, paswword })
+    });
+
+    const result = await response.json();
+
+    document.getElementById("message").innerText = result.message;
+});
