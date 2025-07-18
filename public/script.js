70fedd18da51268ab05bd0e659284ded68c8d6fe
mrdeveloper917 <thakurayush995537@gmail.com>
2025-07-18 22:58:09 +0530

    New File

---
+++ public/script.js
@@ -0,0 +1,11 @@
+document.getElementById('registerForm').addEventListener('submit', async(e) =>{
+    e.preventDefault();
+
+    const name =
+    document.getElementById("name").value ;
+    const email = 
+    document.getElementById("email").value ;
+    const password =
+    document.getElementById("password").value ;
+    
+})
